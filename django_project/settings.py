4a3a58ebb2ae92877a64bbacf8d29cc087b960a8
simon <simonbensoussan.sb@gmail.com>
2017-07-19 13:53:41 +0000

    api_rest app in tdd

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
-    'landing.apps.LandingConfig'
+    'api_rest',
+    'rest_framework'
     
 ]
 
