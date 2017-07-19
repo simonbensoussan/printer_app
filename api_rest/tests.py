a557dbb427c06b009822b63c9e7b2ea52b16a881
simon <simonbensoussan.sb@gmail.com>
2017-07-19 13:58:38 +0000

    add test-TDD & Model

--- api_rest/tests.py
+++ api_rest/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from .models import Bucketlist
 # Create your tests here.
+
+class ModelTestCase(TestCase):
+    """This class defines test suite odf bucketlist model"""
+    
+    def setUp(self):
+        """ Define the test clents and other variables"""
+        self.bucketlist_name = "Class code"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name) #1
+    
+    def test_model_can_create_a_bucketlist(self):
+        """Test the model can create a bucketlist"""
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save() #1
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count,new_count)
+        
+        
+        
